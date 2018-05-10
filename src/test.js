7fc712eca7b3d79f1c42567f2b1298275c6c22d0
Célio Latorraca <celio.latorraca@corp.globo.com>
2018-05-10 14:07:36 -0300

    Check match media queries only when component is already mounted

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 
 import withMatchMediaProps from './'
 
@@ -14,7 +14,7 @@ describe('withMatchMediaProps', () => {
   const Component = () => <div />
   const renderComponent = ({ matchMedias, props }) => {
     const ComponentWithMatchMedia = withMatchMediaProps(matchMedias)(Component)
-    return shallow(<ComponentWithMatchMedia {...props} />)
+    return mount(<ComponentWithMatchMedia {...props} />)
   }
 
   it('forwards all other props', () => {
@@ -22,51 +22,71 @@ describe('withMatchMediaProps', () => {
     expect(component.props()).toEqual({ test: 'prop' })
   })
 
-  it('inject props matched by media query', () => {
-    mockMatchMedia(['min-width: 1024px'])
+  describe('when unmounted', () => {
+    it('does not inject any match media prop', () => {
+      mockMatchMedia(['min-width: 1024px'])
 
-    const component = renderComponent({
-      matchMedias: {
-        'max-width: 1023px': { isMobile: true },
-        'min-width: 1024px': { isDesktop: true }
-      }
+      const component = renderComponent({
+        matchMedias: {
+          'max-width: 1023px': { isMobile: true },
+          'min-width: 1024px': { isDesktop: true }
+        }
+      })
+      component.setState({ mounted: false })
+      expect(component.find(Component).props()).toEqual({})
     })
-    expect(component.props()).toEqual({ isDesktop: true })
   })
 
-  it('keeps all matched media query props', () => {
-    mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
+  describe('when mounted', () => {
+    it('inject props matched by media query', () => {
+      mockMatchMedia(['min-width: 1024px'])
 
-    const component = renderComponent({
-      matchMedias: {
-        'mix-width: 768px': { media768: true },
-        'min-width: 1024px': { media1024: true }
-      }
+      const component = renderComponent({
+        matchMedias: {
+          'max-width: 1023px': { isMobile: true },
+          'min-width: 1024px': { isDesktop: true }
+        }
+      })
+      expect(component.find(Component).props()).toEqual({ isDesktop: true })
     })
-    expect(component.props()).toEqual({ media768: true, media1024: true })
-  })
 
-  it('overrides matched media query props', () => {
-    mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
+    it('keeps all matched media query props', () => {
+      mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
 
-    const component = renderComponent({
-      matchMedias: {
-        'mix-width: 768px': { items: 3 },
-        'min-width: 1024px': { items: 4 }
-      }
+      const component = renderComponent({
+        matchMedias: {
+          'mix-width: 768px': { media768: true },
+          'min-width: 1024px': { media1024: true }
+        }
+      })
+      expect(component.find(Component).props()).toEqual({
+        media768: true,
+        media1024: true
+      })
     })
-    expect(component.props()).toEqual({ items: 4 })
-  })
 
-  it('keeps the last matched media query overrided props, just as css', () => {
-    mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
+    it('overrides matched media query props', () => {
+      mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
 
-    const component = renderComponent({
-      matchMedias: {
-        'min-width: 1024px': { items: 4 },
-        'mix-width: 768px': { items: 3 }
-      }
+      const component = renderComponent({
+        matchMedias: {
+          'mix-width: 768px': { items: 3 },
+          'min-width: 1024px': { items: 4 }
+        }
+      })
+      expect(component.find(Component).props()).toEqual({ items: 4 })
     })
-    expect(component.props()).toEqual({ items: 3 })
+
+    it('keeps the last matched media query overrided props, just as css', () => {
+      mockMatchMedia(['mix-width: 768px', 'min-width: 1024px'])
+
+      const component = renderComponent({
+        matchMedias: {
+          'min-width: 1024px': { items: 4 },
+          'mix-width: 768px': { items: 3 }
+        }
+      })
+      expect(component.find(Component).props()).toEqual({ items: 3 })
+    })
   })
 })
